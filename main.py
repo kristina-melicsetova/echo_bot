8169f9cc0efbebec37981db9ddbcf86c2a161f65
kristina-melicsetova <70933166+kristina-melicsetova@users.noreply.github.com>
2022-07-20 15:50:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 import asyncio
 from aiogram import Bot, Dispatcher, executor
-from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
 
 loop = asyncio.new_event_loop()
-bot = Bot(BOT_TOKEN, parse_mode='HTML')
-storage = MemoryStorage()
-dp = Dispatcher(bot, loop=loop, storage=storage)
-
-
-async def shutdown(dp):
-    await storage.close()
-    await bot.close
-
+bot  = Bot(BOT_TOKEN, parse_mode='HTML')
+dp   = Dispatcher(bot, loop=loop)
 
 if __name__ == '__main__':
-    from handlers import dp, send_hello
+    from handlers import dp
+    executor.start_polling(dp)
 
-    executor.start_polling(dp, on_startup=send_hello, on_shutdown=shutdown)
+'''
+URL_IPHONE13 = 'https://www.apple.com/iphone-13/'
+URL_MACBOOK  = 'https://www.apple.com/shop/buy-mac/macbook-pro/16-inch'
+URL_AIRPODS  = 'https://www.apple.com/macbook-pro-13/'
+'''
